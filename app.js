b35d6436fd6cac3fa88f0354e370cb7a3ef08721
David Anson <david@dlaa.me>
2018-06-18 22:58:27 -0700

    Use compression to gzip response bodies.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const compression = require("compression");
 const express = require("express");
-const app = express();
 
+const app = express();
+app.use(compression({
+  level: 9,
+  threshold: 0
+}));
 app.use(express.static("static"));
 
 const port = process.env.PORT || 3000;
