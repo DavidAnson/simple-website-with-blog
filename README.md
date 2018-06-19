043334184da74428f857e5210e968541921c2ea0
David Anson <david@dlaa.me>
2018-06-18 22:52:07 -0700

    Use express.static to host static content.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # simple-website-with-blog
-Implements a simple web site that includes a blog
+
+A simple web site with a blog
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static("static"));
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
