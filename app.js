ef75815a3a8a3963fc90bf4a7f8bb0029fc49b88
David Anson <david@dlaa.me>
2018-06-19 22:37:37 -0700

    Add handlers for HTTP 404 and 500.

--- app.js
+++ app.js
@@ -19,5 +19,13 @@ app.use(compression({
 }));
 app.use(express.static("static"));
 
+app.use(function (req, res, next) {
+  res.sendStatus(404);
+});
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
