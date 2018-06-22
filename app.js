98cf3af119c04c015a4f58654488b446d5648f21
David Anson <david@dlaa.me>
2018-06-21 23:20:12 -0700

    Redirect HTTP traffic to HTTPS if SWWB_REDIRECT_TO_HTTPS is set.

--- app.js
+++ app.js
@@ -3,8 +3,28 @@
 const compression = require("compression");
 const express = require("express");
 const helmet = require("helmet");
-
 const app = express();
+
+// Fix protocol for HTTPS requests under iisnode
+app.set("trust proxy", true);
+app.use((req, res, next) => {
+  if (!req.secure && req.headers["x-arr-ssl"] && !req.headers["x-forwarded-proto"]) {
+    req.headers["x-forwarded-proto"] = "https";
+  }
+  next();
+});
+
+// Redirect HTTP traffic to HTTPS
+if (process.env.SWWB_REDIRECT_TO_HTTPS) {
+  app.use((req, res, next) => {
+    if (req.secure) {
+      return next();
+    }
+    return res.redirect(`https://${req.headers.host}${req.originalUrl}`);
+  });
+}
+
+// Add security headers and compression
 app.use(helmet({
   "contentSecurityPolicy": {
     "directives": {
@@ -19,8 +39,11 @@ app.use(compression({
   "level": 9,
   "threshold": 0
 }));
+
+// Handle static content
 app.use(express.static("static"));
 
+// Handle HTTP 404/500
 // eslint-disable-next-line no-unused-vars
 app.use((req, res, next) => {
   res.sendStatus(404);
@@ -32,7 +55,7 @@ app.use((err, req, res, next) => {
   res.sendStatus(500);
 });
 
-// eslint-disable-next-line no-process-env
+// Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
