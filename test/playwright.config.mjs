5d49c0ef4c0b18f04533637e658392409a8005a2
David Anson <david@dlaa.me>
2023-12-09 20:56:11 +0000

    Add npm script "ci" to prepare for GitHub continuous integration workflow, enable Firefox/Safari.

--- test/playwright.config.mjs
+++ test/playwright.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 
-/* eslint-disable capitalized-comments, object-curly-newline, object-curly-spacing */
+/* eslint-disable object-curly-newline, object-curly-spacing */
 
 import { defineConfig, devices } from "@playwright/test";
 
@@ -13,15 +13,15 @@ export default defineConfig({
     {
       "name": "chromium",
       "use": { ...devices["Desktop Chrome"] }
+    },
+    {
+      "name": "firefox",
+      "use": { ...devices["Desktop Firefox"] }
+    },
+    {
+      "name": "webkit",
+      "use": { ...devices["Desktop Safari"] }
     }
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] }
-    // },
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] }
-    // }
   ],
   "webServer": {
     "command": "npm run test",
