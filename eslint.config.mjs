8e5a5ae60e1860586055f251660cef82ff1381a9
David Anson <david@dlaa.me>
2026-08-12 23:05:30 +0000

    Address new ESLint warnings from previous commit.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -47,6 +47,7 @@ export default [
       "prefer-named-capture-group": "off",
       "sort-keys": "off",
 
+      "unicorn/consistent-arrow-return-style": "off",
       "unicorn/name-replacements": [ "error", { "replacements": { "err": false, "props": false, "req": false, "res": false } } ],
       "unicorn/no-array-callback-reference": "off",
       "unicorn/no-null": "off",
